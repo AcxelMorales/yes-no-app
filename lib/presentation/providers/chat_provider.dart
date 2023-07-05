b44b0ad787e044e725324502faed5b3b8d99856d
Acxel Morales <acxelmorales97@gmail.com>
2023-07-05 09:11:48 -0600

    Se agrego scroll animado

--- lib/presentation/providers/chat_provider.dart
+++ lib/presentation/providers/chat_provider.dart
@@ -4,15 +4,31 @@ import 'package:yes_no_app/domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier {
 
+  final chatScrollController = ScrollController();
+
   List<Message> messages = [
     Message(text: 'Hola amor!', fromWho: FromWho.me),
     Message(text: 'Ya regresaste del trabajo?', fromWho: FromWho.me),
   ];
 
   Future<void> sendMessage(String text) async {
+    if (text.isEmpty) return;
+
     final message = Message(text: text, fromWho: FromWho.me);
     messages.add(message);
+
     notifyListeners();
+    moveScrollToBottom();
+  }
+
+  Future<void> moveScrollToBottom() async {
+    await Future.delayed(const Duration(milliseconds: 100));
+
+    chatScrollController.animateTo(
+      chatScrollController.position.maxScrollExtent, 
+      duration: const Duration(milliseconds: 300), 
+      curve: Curves.easeOut
+    );
   }
 
 }
